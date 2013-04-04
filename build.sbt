deaff489d1afd316613ea463be4d4c13a82c7bb7
Pierre Carrier <pierre@gcarrier.fr>
2013-04-03 21:47:53 -0700

    working release

--- build.sbt
+++ build.sbt
@@ -5,6 +5,6 @@ version := "1.0"
 scalaVersion := "2.10.1"
 
 libraryDependencies ++= Seq(
-    "org.littleshoot" % "dnsjava" % "2.1.3",
-    "org.jboss.netty" % "netty" % "3.2.9.Final"
+  "org.littleshoot" % "dnsjava" % "2.1.3",
+  "io.netty" % "netty-all" % "4.0.0.CR1",
 )
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,11 +1,48 @@
-import java.util.concurrent.Executors
+import io.netty.bootstrap.Bootstrap
+import io.netty.buffer.Unpooled
+import io.netty.channel.socket.nio.NioDatagramChannel
+import io.netty.channel.{ChannelHandlerContext, ChannelInboundMessageHandlerAdapter}
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.util.internal.logging.{Log4JLoggerFactory, InternalLoggerFactory}
+import io.netty.channel.socket.DatagramPacket
+import java.net.InetAddress
+import org.apache.log4j.BasicConfigurator
+import org.xbill.DNS._
 
 object Server {
-  def main(args: Array[String]) {
-    val factory = new NioDatagramChannelFactory(Executors.newCachedThreadPool())
-    val bootstrap = new ConnectionlessBootstrap(factory)
-    bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+  val LoopbackIP = InetAddress.getByAddress(Array[Byte](8, 8, 8, 8))
 
+  def main(args: Array[String]): Unit = {
+    BasicConfigurator.configure
+    InternalLoggerFactory setDefaultFactory new Log4JLoggerFactory
+
+    val bootstrap = new Bootstrap
+    bootstrap.group(new NioEventLoopGroup).channel(classOf[NioDatagramChannel])
+
+    bootstrap handler new ChannelInboundMessageHandlerAdapter[DatagramPacket] {
+      override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) {
+        cause.printStackTrace()
+        ctx.channel.close
+      }
+
+      def messageReceived(ctx: ChannelHandlerContext, msg: DatagramPacket) {
+        val payload = new Array[Byte](msg.data.readableBytes)
+        msg.data.readBytes(payload)
+        val req = new Message(payload)
+        val rep = new Message
+        rep.getHeader setID req.getHeader.getID
+        rep.getHeader setFlag Flags.QR
+        rep addRecord(req.getQuestion, Section.QUESTION)
+        rep.getQuestion.getRRsetType match {
+          case Type.A => {
+            rep.getHeader setFlag Flags.AA
+            rep addRecord(new ARecord(req.getQuestion.getName, DClass.IN, 0L, LoopbackIP), Section.ANSWER)
+          }
+          case _ => rep.getHeader setRcode Rcode.NOTIMP
+        }
+        ctx.write(new DatagramPacket(Unpooled.copiedBuffer(rep.toWire), msg.remoteAddress()))
+      }
     }
+    bootstrap bind 53
   }
 }
